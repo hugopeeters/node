dcc96248f212c4df5aa3d508df2edcbc3ab5a960
hugopeeters <hpeeters@me.com>
2019-12-31 19:27:16 +0100

    merge two version

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+//include dependencies
 const express = require('express');
-const app = express();
-const port = 3000;
 
+//define app, port and folder
+const app = express();
+app.listen(5000, () => console.log("Listening on port 5000"));
 app.use(express.static('public'));
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(express.json({ limit: '1mb' }));
+
+app.post('/api', (request, response) => {
+    console.log(request.body);
+    response.json({
+        status: "success",
+        latitude: request.body.latitude,
+        longitude: request.body.longitude
+    });
+});
